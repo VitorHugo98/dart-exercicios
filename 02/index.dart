eccf6aa1783a5b0b4fd74f79c264476be5baf074
VitorHugo98 <vytorpet@gmail.com>
2023-11-16 13:25:10 -0300

    Exercícios 2 e 3

--- 02/index.dart
+++ 02/index.dart
@@ -1,1 +1,22 @@
-const letras = ["A", "a", "B", "C", "L", "z"];
+ 
+
+void contarletras (List <String>letras){
+  int quantidade = letras.where((a) => a.toLowerCase() == 'e').length;
+  if(quantidade>0){
+    print('foram encontradas $quantidade letras "e" ou "E".');
+  }
+  else {
+    print('nenhuma letra "E" ou "e" foi encontrada.');
+  }
+
+}
+
+void main(){
+  const letras1 = ["A", "a", "B", "C", "L", "z"];
+  const letras2 =["A", "e", "B", "C", "E", "e","z"];
+  contarletras(letras1);
+  print(letras1);
+  contarletras(letras2);
+  print(letras2);
+
+}
--- 03/index.dart
+++ 03/index.dart
@@ -1,1 +1,26 @@
-const numeros = [54, 22, 14, 87, 284];
+bool identificador(List<int>numeros)
+{
+  int valor=10; 
+  int indice = numeros.indexOf(valor);
+    bool condicao = indice == -1; 
+    return condicao;
+     
+  }
+  void main (){
+    const numeros1 = [54, 22, 14, 87, 284];
+  
+    const numeros2 = [54, 22, 14, 10, 284];
+    
+  if(identificador(numeros2)){
+    print("não existe");
+  }else{
+    print("existe");
+  }
+ 
+
+   
+  }
+
+
+
+
