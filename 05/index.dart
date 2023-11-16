3442e96888afd80ce2377c44fc2202b3572b15c8
VitorHugo98 <vytorpet@gmail.com>
2023-11-16 15:04:34 -0300

    exercícios 5 e 6

--- 05/index.dart
+++ 05/index.dart
@@ -1,1 +1,8 @@
-const original = [1, 4, 12, 21, 53, 88, 112];
+const original = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+void main(){
+  for(int a=0; a<=original.length-1; a++  ){
+    if(a%2!=0){
+      print(original[a]);
+    }
+    
+  }}
